04762df6cc4a66a6035b3491cd65f65e77901fba
bosko <bosko@d64>
2016-05-30 16:31:17 +0200

    Basic interface for using safe checkpoints

---
+++ src/main/java/com/booking/replication/checkpoints/SafeCheckpointType.java
@@ -0,0 +1,9 @@
+package com.booking.replication.checkpoints;
+
+/**
+ * Created by bosko on 5/30/16.
+ */
+public class SafeCheckpointType {
+    public static final int BINLOG_FILENAME = 1;
+    public static final int GTID = 2;
+}

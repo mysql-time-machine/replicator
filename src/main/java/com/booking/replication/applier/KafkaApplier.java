56426ef2c9ab36ac6045f263119cd0dac02107fd
Raynald Chung <lei.zhong@booking.com>
2016-06-08 17:29:46 +0200

    Description:  - First try of Kafka applier, print event json to stdout

--- src/main/java/com/booking/replication/applier/KafkaApplier.java
+++ src/main/java/com/booking/replication/applier/KafkaApplier.java
@@ -1,26 +1,51 @@
 package com.booking.replication.applier;
 
 import com.booking.replication.Configuration;
+import com.booking.replication.augmenter.AugmentedRow;
 import com.booking.replication.augmenter.AugmentedRowsEvent;
 import com.booking.replication.augmenter.AugmentedSchemaChangeEvent;
+import com.booking.replication.util.MutableLong;
 import com.booking.replication.pipeline.PipelineOrchestrator;
+
 import com.google.code.or.binlog.impl.event.FormatDescriptionEvent;
 import com.google.code.or.binlog.impl.event.QueryEvent;
 import com.google.code.or.binlog.impl.event.RotateEvent;
 import com.google.code.or.binlog.impl.event.XidEvent;
 
+import java.util.*;
 import java.io.IOException;
 
 /**
- * Created by rmirica on 08/06/16.
+ * Created by raynald on 08/06/16.
  */
+
 public class KafkaApplier implements Applier {
+    private final com.booking.replication.Configuration replicatorConfiguration;
+    private static long totalEventsCounter = 0;
+    private static long totalRowsCounter = 0;
+
+    private static final HashMap<String, MutableLong> stats = new HashMap<String, MutableLong>();
+
     public KafkaApplier(Configuration configuration) {
+        replicatorConfiguration = configuration;
     }
 
     @Override
     public void applyAugmentedRowsEvent(AugmentedRowsEvent augmentedSingleRowEvent, PipelineOrchestrator caller) throws IOException {
+        totalEventsCounter ++;
 
+        for(AugmentedRow row : augmentedSingleRowEvent.getSingleRowEvents()) {
+            String tableName = row.getTableName();
+            if (tableName != null) {
+                totalRowsCounter ++;
+                if (stats.containsKey(tableName)) {
+                    stats.get(tableName).increment();
+                } else {
+                    stats.put(tableName, new MutableLong());
+                }
+            }
+            System.out.println(row.toJSON());
+        }
     }
 
     @Override

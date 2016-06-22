e0c4663081e3a2dccca7fb160340d4c0c87052eb
Rares Mirica <rares.mirica@booking.com>
2016-06-22 11:06:02 +0200

    Implemented Kafka client metrics collector

---
+++ src/main/java/com/booking/replication/applier/KafkaMetricsCollector.java
@@ -0,0 +1,70 @@
+package com.booking.replication.applier;
+
+import com.booking.replication.Metrics;
+import com.codahale.metrics.Gauge;
+import org.apache.kafka.common.MetricName;
+import org.apache.kafka.common.metrics.KafkaMetric;
+import org.apache.kafka.common.metrics.MetricsReporter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.codahale.metrics.MetricRegistry.name;
+
+class KafkaMetricsCollector implements MetricsReporter {
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaMetricsCollector.class);
+
+    private Map<KafkaMetric, Boolean> monitored = new HashMap<>();
+
+    @Override
+    public void init(List<KafkaMetric> metrics) {
+        for (KafkaMetric metric: metrics) {
+            if (!monitored.containsKey(metric)) {
+                monitored.put(metric, true);
+                LOGGER.debug("Adding kafka metric: " + sanitizeName(metric.metricName()));
+                Metrics.registry.register(name("Kafka", sanitizeName(metric.metricName())), new KafkaMetricGauge(metric));
+            }
+        }
+    }
+
+    @Override
+    public void metricChange(KafkaMetric metric) {
+        if (!monitored.containsKey(metric)) {
+            monitored.put(metric, true);
+            LOGGER.debug("Adding kafka metric: " + sanitizeName(metric.metricName()));
+            Metrics.registry.register(name("Kafka", sanitizeName(metric.metricName())), new KafkaMetricGauge(metric));
+        }
+    }
+
+    private static class KafkaMetricGauge implements Gauge<Double> {
+        private static final Logger LOGGER = LoggerFactory.getLogger(KafkaMetricGauge.class);
+
+        private KafkaMetric metric;
+
+        KafkaMetricGauge(KafkaMetric metric) {
+            this.metric = metric;
+        }
+
+        @Override
+        public Double getValue() {
+            return metric.value();
+        }
+    }
+
+    private String sanitizeName(MetricName name) {
+        StringBuilder result = new StringBuilder().append(name.group()).append('.');
+        for (Map.Entry<String, String> tag : name.tags().entrySet()) {
+            result.append(tag.getValue()).append('.');
+        }
+        return result.append(name.name()).toString().replace(' ', '_').replace("\\.", "_");
+    }
+
+    @Override
+    public void close() {}
+
+    @Override
+    public void configure(Map<String, ?> configs) {}
+}

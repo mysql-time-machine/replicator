15dba18aa1aab750ebd45625e94dd9616aef2e2b
Rares Mirica <rares.mirica@booking.com>
2016-07-13 12:22:12 +0200

    Add binlog position to safe checkpoint

--- src/main/java/com/booking/replication/checkpoints/LastVerifiedBinlogFile.java
+++ src/main/java/com/booking/replication/checkpoints/LastVerifiedBinlogFile.java
@@ -44,13 +44,21 @@ public class LastVerifiedBinlogFile implements SafeCheckPoint {
         return this.checkpointType;
     }
 
+    public int getSlaveId() {
+        return slaveId;
+    }
+
+    public void setSafeCheckPointPosition( long lastVerifiedBinlogPosition) {
+        this.lastVerifiedBinlogPosition = lastVerifiedBinlogPosition;
+    }
+
+    public Long getSafeCheckPointPosition() {
+        return  lastVerifiedBinlogPosition;
+    }
+
     @Override
     public String getSafeCheckPointMarker() {
         return lastVerifiedBinlogFileName;
-    }
-
-    public int getSlaveId() {
-        return slaveId;
     }
 
     @Override
--- src/main/java/com/booking/replication/checkpoints/SafeCheckPoint.java
+++ src/main/java/com/booking/replication/checkpoints/SafeCheckPoint.java
@@ -1,9 +1,11 @@
 package com.booking.replication.checkpoints;
 
+import java.io.Serializable;
+
 /**
  * Created by bosko on 5/30/16.
  */
-public interface SafeCheckPoint {
+public interface SafeCheckPoint extends Serializable {
 
     public int getCheckpointType();
 

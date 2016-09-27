3bda865e842581016f6921c599e457bb1a78e620
Mikhail Dutikov <mikhail.dutikov@gmail.com>
2016-09-27 13:17:20 +0200

    A couple more tests

--- src/test/java/com/booking/replication/util/MonotonicPartialFunctionSearchTest.java
+++ src/test/java/com/booking/replication/util/MonotonicPartialFunctionSearchTest.java
@@ -40,4 +40,36 @@ public class MonotonicPartialFunctionSearchTest {
 
         assertEquals(null, f.reverseGLB("2000"));
     }
+
+    @Test
+    public void makeSureWeCanPullThruUndefinedFunctions()
+    {
+        String[] namesOfFiles = {"1", "2", "3", "4", "5", "6", "7", "8"};
+        HashMap<String, String> fileNameToFirstGtidInThatFile = new HashMap<>();
+
+        fileNameToFirstGtidInThatFile.put("1", "1000");
+        fileNameToFirstGtidInThatFile.put("2", null);
+        fileNameToFirstGtidInThatFile.put("3", null);
+        fileNameToFirstGtidInThatFile.put("4", "2000");
+        fileNameToFirstGtidInThatFile.put("5", null);
+        fileNameToFirstGtidInThatFile.put("6", "4000");
+        fileNameToFirstGtidInThatFile.put("7", null);
+        fileNameToFirstGtidInThatFile.put("8", "6000");
+
+        MonotonicPartialFunctionSearch<String, String> f = new MonotonicPartialFunctionSearch<>(
+                x -> fileNameToFirstGtidInThatFile.get(x), namesOfFiles);
+
+        assertEquals("4", f.reverseGLB("3000"));
+    }
+
+    @Test
+    public void makeSureWeProduceNoResultsIfFunctionValueIsNotDefinedForAnything()
+    {
+        String[] namesOfFiles = {"1", "2", "3", "4", "5", "6", "7", "8"};
+
+        MonotonicPartialFunctionSearch<String, String> f = new MonotonicPartialFunctionSearch<>(
+                x -> null, namesOfFiles);
+
+        assertEquals(null, f.reverseGLB("3000"));
+    }
 }

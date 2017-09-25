fa36b5f881fe0ea211e7b3c9788bc07d9ef8c799
Evgeny Dmitriev <evgeny.dmitriev@booking.com>
2017-09-25 12:18:19 +0200

    Fixes a fail when a query event with a comment in the begging of the sql-string arrives.

--- src/main/java/com/booking/replication/sql/QueryPatterns.java
+++ src/main/java/com/booking/replication/sql/QueryPatterns.java
@@ -4,12 +4,12 @@ package com.booking.replication.sql;
  * Created by bosko on 8/30/16.
  */
 public class QueryPatterns {
-    public static final String isBEGIN    = "^(begin)";
-    public static final String isCOMMIT   = "^(commit)";
-    public static final String isDDLDefiner = "^(alter|drop|create|rename|truncate|modify)\\s+definer\\s*=";
-    public static final String isDDLTable = "^(alter|drop|create|rename|truncate|modify)\\s+(table)";
-    public static final String isDDLTemporaryTable = "^(alter|drop|create|rename|truncate|modify)\\s+temporary\\s+(table)";
-    public static final String isDDLView  = "^(alter|drop|create|rename|truncate|modify)\\s+(view)";
-    public static final String isANALYZE  = "^(analyze)\\s+(table)";
+    public static final String isBEGIN    = "^(/\\*.*?\\*/\\s*)?(begin)";
+    public static final String isCOMMIT   = "^(/\\*.*?\\*/\\s*)?(commit)";
+    public static final String isDDLDefiner = "^(/\\*.*?\\*/\\s*)?(alter|drop|create|rename|truncate|modify)\\s+definer\\s*=";
+    public static final String isDDLTable = "^(/\\*.*?\\*/\\s*)?(alter|drop|create|rename|truncate|modify)\\s+(table)";
+    public static final String isDDLTemporaryTable = "^(/\\*.*?\\*/\\s*)?(alter|drop|create|rename|truncate|modify)\\s+temporary\\s+(table)";
+    public static final String isDDLView  = "^(/\\*.*?\\*/\\s*)?(alter|drop|create|rename|truncate|modify)\\s+(view)";
+    public static final String isANALYZE  = "^(/\\*.*?\\*/\\s*)?(analyze)\\s+(table)";
     public static final String isGTIDPattern  = "(?<=_pseudo_gtid_hint__asc\\:)(.{8}\\:.{16}\\:.{8})";
 }

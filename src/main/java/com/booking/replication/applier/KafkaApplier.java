cca60e13063cf79f68d55064213b29405729fa94
Fabricio Damasceno <fabricio.damasceno@booking.com>
2018-05-28 13:42:46 +0200

    Changing the max.request.size from 1MB to 5MB

--- src/main/java/com/booking/replication/applier/KafkaApplier.java
+++ src/main/java/com/booking/replication/applier/KafkaApplier.java
@@ -96,7 +96,8 @@ public class KafkaApplier implements Applier {
         prop.put("retries", 30); // Default value: 0
         prop.put("batch.size", 16384); // Default value: 16384
         // prop.put("linger.ms", 20); // Default 0, Artificial delay
-        prop.put("buffer.memory", 33554432); // Default value: 33554432
+        // prop.put("buffer.memory", 33554432); // Default value: 33554432
+        prop.put("max.request.size", 5242880); // Default value: 1048576
         prop.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         prop.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         prop.put("metric.reporters", "com.booking.replication.applier.KafkaMetricsCollector");

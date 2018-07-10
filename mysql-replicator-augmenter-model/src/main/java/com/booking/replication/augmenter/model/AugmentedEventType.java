8676e350f9dea723e3c3a494e8bd0bff15ccf4da
Fabricio Damasceno <fabricio.damasceno@booking.com>
2018-07-10 17:23:40 +0200

    Adding the definition to the AugmentedEventType to help clients to desserialize the data

--- mysql-replicator-augmenter-model/src/main/java/com/booking/replication/augmenter/model/AugmentedEventType.java
+++ mysql-replicator-augmenter-model/src/main/java/com/booking/replication/augmenter/model/AugmentedEventType.java
@@ -4,20 +4,27 @@ import java.io.Serializable;
 
 @SuppressWarnings("unused")
 public enum AugmentedEventType implements Serializable {
-    BYTE_ARRAY(0),
-    WRITE_ROWS(1),
-    UPDATE_ROWS(2),
-    DELETE_ROWS(3),
-    QUERY(4);
+    BYTE_ARRAY(0, ByteArrayAugmentedEventData.class),
+    WRITE_ROWS(1, WriteRowsAugmentedEventData.class),
+    UPDATE_ROWS(2, UpdateRowsAugmentedEventData.class),
+    DELETE_ROWS(3, DeleteRowsAugmentedEventData.class),
+    QUERY(4, QueryAugmentedEventData.class);
 
     private final int code;
+    private final Class<? extends AugmentedEventData> definition;
 
-    AugmentedEventType(int code) {
+    AugmentedEventType(int code, Class<? extends AugmentedEventData> definition) {
         this.code = code;
+        this.definition = definition;
     }
 
     public int getCode() {
         return this.code;
+    }
+
+    // used by clients to deserialize the data
+    public Class<? extends AugmentedEventData> getDefinition() {
+        return this.definition;
     }
 }
 

7668d276b884482210622f3c0629541bb519f1d0
Fabricio Damasceno <fabricio.damasceno@booking.com>
2018-11-28 11:13:15 +0100

    Changing the kafka producer configurations back to default

--- src/main/java/com/booking/replication/applier/KafkaApplier.java
+++ src/main/java/com/booking/replication/applier/KafkaApplier.java
@@ -92,17 +92,17 @@ public class KafkaApplier implements Applier {
         // Below is the new version of producer configuration
         Properties prop = new Properties();
         prop.put("bootstrap.servers", broker);
-        prop.put("acks", "all"); // Default 1
-        prop.put("retries", 30); // Default value: 0
-        prop.put("batch.size", 8192); // Default value: 16384
+        // prop.put("acks", "all"); // Default 1
+        // prop.put("retries", 30); // Default value: 0
+        // prop.put("batch.size", 8192); // Default value: 16384
         // prop.put("linger.ms", 20); // Default 0, Artificial delay
         // prop.put("buffer.memory", 33554432); // Default value: 33554432
-        prop.put("max.request.size", 10485760); // Default value: 1048576
-        prop.put("max.block.ms", 600000); // Default value: 60000
+        // prop.put("max.request.size", 10485760); // Default value: 1048576
+        // prop.put("max.block.ms", 600000); // Default value: 60000
         prop.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         prop.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         prop.put("metric.reporters", "com.booking.replication.applier.KafkaMetricsCollector");
-        prop.put("request.timeout.ms", 100000);
+        // prop.put("request.timeout.ms", 100000);
         return prop;
     }
 

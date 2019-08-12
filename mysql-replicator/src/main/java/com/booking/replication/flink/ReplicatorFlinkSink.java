2d201cc27fff4c2f6c833d98be0ab5b9c917c281
Bosko Devetak <bosko.devetak@gmail.com>
2019-08-12 18:26:44 +0200

    add checkpoint notifiy and some debug messages in sink class

--- mysql-replicator/src/main/java/com/booking/replication/flink/ReplicatorFlinkSink.java
+++ mysql-replicator/src/main/java/com/booking/replication/flink/ReplicatorFlinkSink.java
@@ -5,6 +5,7 @@ import com.booking.replication.augmenter.model.event.AugmentedEvent;
 import com.booking.replication.commons.checkpoint.Checkpoint;
 import org.apache.flink.api.common.state.ListState;
 import org.apache.flink.api.common.state.ListStateDescriptor;
+import org.apache.flink.runtime.state.CheckpointListener;
 import org.apache.flink.runtime.state.FunctionInitializationContext;
 import org.apache.flink.runtime.state.FunctionSnapshotContext;
 import org.apache.flink.streaming.api.checkpoint.CheckpointedFunction;
@@ -15,7 +16,7 @@ import java.util.Map;
 
 public class ReplicatorFlinkSink
         extends RichSinkFunction<Collection<AugmentedEvent>>
-        implements CheckpointedFunction {
+        implements CheckpointedFunction, CheckpointListener {
 
     private transient Applier applier;
     private Map<String,Object> configuration;
@@ -28,7 +29,9 @@ public class ReplicatorFlinkSink
     }
 
     @Override
-    public void invoke(Collection<AugmentedEvent> augmentedEvents) throws Exception {
+    public void invoke(Collection<AugmentedEvent> augmentedEvents, Context context) throws Exception {
+
+        System.out.println("applier collection size => " + augmentedEvents.size());
 
         if (applier == null) {
             System.out.println("Lost applier");
@@ -37,12 +40,23 @@ public class ReplicatorFlinkSink
         applier.apply(augmentedEvents);
 
         this.binlogCheckpoint =
-                augmentedEvents.stream().findFirst().get().getHeader().getCheckpoint();
+                augmentedEvents
+                        .stream()
+                        .findFirst()
+                        .get()
+                        .getHeader()
+                        .getCheckpoint();
 
+        if (this.binlogCheckpoint != null && this.binlogCheckpoint.getGtidSet() != null) {
+            System.out.println("applier -> " + this.binlogCheckpoint.getGtidSet());
+        }
     }
 
     @Override
     public void snapshotState(FunctionSnapshotContext functionSnapshotContext) throws Exception {
+
+        System.out.println("sink snapshotState, checkpointID => " + functionSnapshotContext.getCheckpointId());
+
         if (binlogCheckpoint != null) {
             if ( binlogCheckpoint.getGtidSet() != null) {
                 System.out.println("BinlogSink: snapshotting state, gtidSet #" + binlogCheckpoint.getGtidSet());
@@ -50,13 +64,16 @@ public class ReplicatorFlinkSink
                 this.binlogCheckpoints.add(binlogCheckpoint);
             }
         }
+
     }
 
     @Override
     public void initializeState(FunctionInitializationContext functionInitializationContext) throws Exception {
         this.applier = Applier.build(configuration);
-        this.binlogCheckpoints = functionInitializationContext
-                .getOperatorStateStore()
-                .getListState(new ListStateDescriptor<>("binlogCheckpoint", Checkpoint.class));
+    }
+
+    @Override
+    public void notifyCheckpointComplete(long l) throws Exception {
+        System.out.println("sink checkpointComplete -> " + l);
     }
 }

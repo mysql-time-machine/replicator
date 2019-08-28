1ec5ed4eea7bfaf42565ab57058410101fbfa173
Nageswaran Sokkayaraj <nageswaran.sokkayaraj@booking.com>
2019-08-28 15:15:14 +0200

    Uncommenting Integration tests

--- mysql-replicator/src/test/java/com/booking/replication/it/hbase/ReplicatorHBasePipelineIntegrationTestRunner.groovy
+++ mysql-replicator/src/test/java/com/booking/replication/it/hbase/ReplicatorHBasePipelineIntegrationTestRunner.groovy
@@ -87,14 +87,13 @@ class ReplicatorHBasePipelineIntegrationTestRunner extends Specification {
     @Shared public static final String  BIGTABLE_INSTANCE = getBigTableInstance()
 
     @Shared private TESTS = [
-//            new TableWhiteListTest(),
-//            new TableNameMergeFilterTestImpl(),
-//            new TransmitInsertsTestImpl(),
-//            new MicrosecondValidationTestImpl(),
-//            new LongTransactionTestImpl(),
-//            new PayloadTableTestImpl(),
-//            new SplitTransactionTestImpl()
-              new SplitTransactionTestImpl()
+            new TableWhiteListTest(),
+            new TableNameMergeFilterTestImpl(),
+            new TransmitInsertsTestImpl(),
+            new MicrosecondValidationTestImpl(),
+            new LongTransactionTestImpl(),
+            new PayloadTableTestImpl(),
+            new SplitTransactionTestImpl()
     ]
 
     @Shared ServicesProvider servicesProvider = ServicesProvider.build(ServicesProvider.Type.CONTAINERS)

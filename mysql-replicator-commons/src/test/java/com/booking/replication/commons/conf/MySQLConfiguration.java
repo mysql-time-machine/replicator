31bd29757e903facbfc8b0cb60ccaa5c4638ed3a
Steve Aurigema <steve.aurigema@gmail.com>
2020-03-02 08:33:34 +0100

    Add default values into the MySQLConfiguration class. These defaults (or some variation thereof) are present in a number of different files, this commit centralises them and introduces both an empty constructor (to make use of the new defaults) or a single argument constructor for only overwriting the init scripts.

--- mysql-replicator-commons/src/test/java/com/booking/replication/commons/conf/MySQLConfiguration.java
+++ mysql-replicator-commons/src/test/java/com/booking/replication/commons/conf/MySQLConfiguration.java
@@ -2,6 +2,7 @@ package com.booking.replication.commons.conf;
 
 import org.testcontainers.containers.Network;
 
+import java.util.Collections;
 import java.util.List;
 
 public class MySQLConfiguration {
@@ -12,6 +13,15 @@ public class MySQLConfiguration {
     private List<String> initScripts;
     private Network network;
     private String networkAlias;
+
+    private final String DEFAULT_SCHEMA     = "test";
+    private final String DEFAULT_USERNAME   = "replicator";
+    private final String DEFAULT_PASSWORD   = "replicator";
+    private final String DEFAULT_CONF_PATH  = "my.cnf";
+    private final Network DEFAULT_NETWORK   = null;
+    private final String DEFAULT_NETWORK_ALIAS = "";
+    private final List<String> DEFAULT_INIT_SCRIPTS = Collections.emptyList();
+
 
     public MySQLConfiguration(String schema,
                               String username,
@@ -27,6 +37,21 @@ public class MySQLConfiguration {
         this.initScripts = initScripts;
         this.network = network;
         this.networkAlias = networkAlias;
+    }
+
+    public MySQLConfiguration() {
+        this.schema   = DEFAULT_SCHEMA;
+        this.username = DEFAULT_USERNAME;
+        this.password = DEFAULT_PASSWORD;
+        this.confPath = DEFAULT_CONF_PATH;
+        this.initScripts = DEFAULT_INIT_SCRIPTS;
+        this.network     = DEFAULT_NETWORK;
+        this.networkAlias= DEFAULT_NETWORK_ALIAS;
+    }
+
+    public MySQLConfiguration (List<String> initScripts) {
+        this();
+        this.initScripts = initScripts;
     }
 
     public Network getNetwork() {
